89285eab4ac24840fbeda1b13e89aecdef42d0b4
IamSila <silamulingi@gmail.com>
2025-11-20 23:28:21 +0300

    a readme of what I learnt from the book

--- CHAPTER_1/pointers_and_memory.c
+++ CHAPTER_1/pointers_and_memory.c
@@ -6,10 +6,17 @@
  */
 
 int main(void) {
+	int num;
+	int *pi = &num;
+
+
 	// getting the character from an array using pointer method
 	char *names[] = {"Miller", "Sila", "Kenya"};
 	printf("%c",*(*(names + 1) + 1));
 	//method 2
 	printf("%c", names[1][2]);
+
+	// Avoiding the conflict in different platforms
+	printf("The pointer is :%p", (void *)pi);
 	return (0);
 }
