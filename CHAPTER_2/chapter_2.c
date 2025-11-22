cbb2d3ff70a74b1f370bc292b22a0a9b9ffdd26d
IamSila <silamulingi@gmail.com>
2025-11-22 22:34:30 +0300

    dynamic memory allocation in C

--- CHAPTER_2/chapter_2.c
+++ CHAPTER_2/chapter_2.c
@@ -11,6 +11,9 @@ int main(void) {
 	/**
 	 * dynamic memory management
 	 */
-	
+	int *pi = (int *) malloc(sizeof(int));
+	*pi = 5;
+	printf("The value of pi is: %d", *pi);
+	free(pi);
 	return(0)
 }
